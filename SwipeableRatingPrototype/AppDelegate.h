836be72bc7a160a08b4d58f4e946a9f789ed5ee0
Nicholas Wong <nickwph@gmail.com>
2014-03-03 19:36:37 -0500

    Initial commit.

---
+++ SwipeableRatingPrototype/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SwipeableRatingPrototype
+//
+//  Created by Nicholas Wong on 3/3/14.
+//  Copyright (c) 2014 Nicholas Workshop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SwipeableRatingPrototype/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SwipeableRatingPrototype
+//
+//  Created by Nicholas Wong on 3/3/14.
+//  Copyright (c) 2014 Nicholas Workshop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
